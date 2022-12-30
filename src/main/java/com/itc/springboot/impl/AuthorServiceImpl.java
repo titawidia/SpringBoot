2fb2e14ccf94310b95d5a627a6801a3af9de8793
titawidia <gitawidia166@gmail.com>
2022-12-30 10:09:40 +0700

    Pengenalan Jpa Repository 2

--- src/main/java/com/itc/springboot/impl/AuthorServiceImpl.java
+++ src/main/java/com/itc/springboot/impl/AuthorServiceImpl.java
@@ -1,15 +1,28 @@
 package com.itc.springboot.impl;
 
+import com.itc.springboot.domain.Author;
 import com.itc.springboot.dto.AuthorResponseDTO;
+import com.itc.springboot.exception.BadRequestException;
+import com.itc.springboot.repository.AuthorRepository;
 import com.itc.springboot.service.AuthorService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+
     @Override
     public AuthorResponseDTO findAuthorById(Long id) {
         //1. fetch data from database
+        Author author = authorRepository.findById(id)
+        .orElseThrow(() -> new BadRequestException("invalid.authorId"));
         //2. author -> authorResponseDTO
-        return null;
+        AuthorResponseDTO dto = new AuthorResponseDTO();
+        dto.setAuthorName(author.getName());
+        dto.setBirthDate(author.getBirthDate().toEpochDay());
+        return dto;
     }
 }
---
+++ src/main/java/com/itc/springboot/web/AuthorResource.java
@@ -0,0 +1,22 @@
+package com.itc.springboot.web;
+
+import com.itc.springboot.dto.AuthorResponseDTO;
+import com.itc.springboot.service.AuthorService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class AuthorResource {
+
+    private final AuthorService authorService;
+
+    //author detail
+    @GetMapping("/author/{id}/detail")
+    public ResponseEntity<AuthorResponseDTO> findAuthorById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(authorService.findAuthorById(id));
+    }
+}
