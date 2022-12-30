b365b2723f62985d8795d4c17e3e063be78de1f8
titawidia <gitawidia166@gmail.com>
2022-12-30 16:02:46 +0700

    ManyToMany Relationship: Book - Author

--- src/main/java/com/itc/springboot/domain/Book.java
+++ src/main/java/com/itc/springboot/domain/Book.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @Entity
@@ -28,4 +29,12 @@ public class Book implements Serializable {
     @ManyToOne
     @JoinColumn(name = "publisher_id", nullable = false)
     private Publisher publisher;
+
+    @ManyToMany
+    @JoinTable(name = "book_author", joinColumns = {
+            @JoinColumn(name = "book_id", referencedColumnName = "id")},
+            inverseJoinColumns = {
+            @JoinColumn(name = "author_id", referencedColumnName = "id")
+    })
+    private List<Author> authors;
 }
